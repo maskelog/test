b87ff229ff9ab1e0314e65e8af004a4d1c87e119
maskelog <blitzmaske@gmail.com>
2023-10-08 22:16:10 +0900

    mini에서 boxoffice 변경

---
+++ React/boxoffice/src/MovieList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MovieList({ boxOfficeMovies }) {
+  return (
+    <ul>
+      {boxOfficeMovies.map(movie => (
+        <li key={movie.movieCd}>
+          <h3>{movie.movieNm}</h3>
+          <p>순위: {movie.rank}</p>
+          {movie.poster && <img src={movie.poster} alt={movie.movieNm} />}
+          <p>개봉일: {movie.openDt}</p>
+          <p>누적관람객: {movie.audiAcc}명</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default MovieList;
